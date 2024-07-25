3c583e6bd6cf442f09cb1d605f20d42cae36da78
theketan26 <ketangsolanki129@gmail.com>
2024-07-25 17:52:15 +0530

    test commmit

--- program1.js
+++ program1.js
@@ -3,6 +3,8 @@ const getTotalIsles = function (grid) {
 
   // write your code here
 
+  // test in program 1
+
 };
 
 module.exports = getTotalIsles;
--- program2.js
+++ program2.js
@@ -2,6 +2,8 @@ const decodeTheRing = function (s, p) {
 
     // write your code here
 
+    // test in program 2
+
   };
   
   module.exports = decodeTheRing;
